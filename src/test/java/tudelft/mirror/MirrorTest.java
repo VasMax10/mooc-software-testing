c853c45d996831a4c6c7b5457c309f352523624b
VasMax10 <vasmax10@gmail.com>
2023-02-12 13:24:26 +0200

    Write MirrorTest and Fix the bug in Mirror

--- src/test/java/tudelft/mirror/MirrorTest.java
+++ src/test/java/tudelft/mirror/MirrorTest.java
@@ -1,4 +1,26 @@
 package tudelft.mirror;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 public class MirrorTest {
+    @ParameterizedTest(name = "input={0}, result={1}")
+    @CsvSource({"abXYZba, ab", "abca, a", "aba, aba","abXQba,ab", "a,a","aa,aa","aaa,aaa","aaaa,aaaa"})
+    public void mirrorQuizTest(String input, String expectedResult){
+
+        Mirror mirror = new Mirror();
+        String result = mirror.mirrorEnds(input);
+
+        Assertions.assertEquals(expectedResult, result);
+    }
+
+    @Test
+    public void mirrorEmptyInputTest(){
+        Mirror mirror = new Mirror();
+        String result = mirror.mirrorEnds("");
+
+        Assertions.assertEquals("", result);
+    }
 }
