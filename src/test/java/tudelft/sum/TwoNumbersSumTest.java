0010dfba99860d4379e10484e0ac02a44f0ca9af
VasMax10 <vasmax10@gmail.com>
2023-02-13 19:58:28 +0200

    Write TwoNumbersSumTest and fix the bug in TwoNumbersSum (create new digit)

--- src/test/java/tudelft/sum/TwoNumbersSumTest.java
+++ src/test/java/tudelft/sum/TwoNumbersSumTest.java
@@ -1,5 +1,65 @@
 package tudelft.sum;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class TwoNumbersSumTest {
 
+    @Test
+    public void addSameLengthNumbers(){
+        TwoNumbersSum twoNumbersSum = new TwoNumbersSum();
+        ArrayList<Integer> first = new ArrayList<>(Arrays.asList(1,2));
+        ArrayList<Integer> second = new ArrayList<>(Arrays.asList(3,4));
+
+        ArrayList<Integer> result = twoNumbersSum.addTwoNumbers(first, second);
+
+        Assertions.assertEquals(new ArrayList<>(Arrays.asList(4,6)), result);
+
+    }
+
+    @Test
+    public void addNumbersSecondHasMoreDigits(){
+        TwoNumbersSum twoNumbersSum = new TwoNumbersSum();
+        ArrayList<Integer> first = new ArrayList<>(Arrays.asList(1,2));
+        ArrayList<Integer> second = new ArrayList<>(Arrays.asList(3,4,5));
+
+        ArrayList<Integer> result = twoNumbersSum.addTwoNumbers(first, second);
+
+        Assertions.assertEquals(new ArrayList<>(Arrays.asList(3,5,7)), result);
+    }
+    @Test
+    public void addNumbersFirstHasMoreDigits(){
+        TwoNumbersSum twoNumbersSum = new TwoNumbersSum();
+        ArrayList<Integer> first = new ArrayList<>(Arrays.asList(1,2,3));
+        ArrayList<Integer> second = new ArrayList<>(Arrays.asList(4,5));
+
+        ArrayList<Integer> result = twoNumbersSum.addTwoNumbers(first, second);
+
+        Assertions.assertEquals(new ArrayList<>(Arrays.asList(1,6,8)), result);
+    }
+
+    @Test
+    public void addNumbersDigitSumMoreThan10(){
+        TwoNumbersSum twoNumbersSum = new TwoNumbersSum();
+        ArrayList<Integer> first = new ArrayList<>(Arrays.asList(1,9));
+        ArrayList<Integer> second = new ArrayList<>(Arrays.asList(9));
+
+        ArrayList<Integer> result = twoNumbersSum.addTwoNumbers(first, second);
+
+        Assertions.assertEquals(new ArrayList<>(Arrays.asList(2,8)), result);
+    }
+
+    @Test
+    public void addNumbersThatCreatesNewDigit(){
+        TwoNumbersSum twoNumbersSum = new TwoNumbersSum();
+        ArrayList<Integer> first = new ArrayList<>(Arrays.asList(9));
+        ArrayList<Integer> second = new ArrayList<>(Arrays.asList(9));
+
+        ArrayList<Integer> result = twoNumbersSum.addTwoNumbers(first, second);
+
+        Assertions.assertEquals(new ArrayList<>(Arrays.asList(1,8)), result);
+    }
 }
