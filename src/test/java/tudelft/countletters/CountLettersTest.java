be8716a550bd858671c696e71a70622d6246da37
VasMax10 <vasmax10@gmail.com>
2023-02-13 14:13:29 +0200

    Write 100% coverage CountLettersTest and fix the bug in CointLetters

--- src/test/java/tudelft/countletters/CountLettersTest.java
+++ src/test/java/tudelft/countletters/CountLettersTest.java
@@ -6,14 +6,26 @@ import org.junit.jupiter.api.Test;
 public class CountLettersTest {
 
     @Test
-    public void multipleMatchingWords() {
+    public void multipleMatchingSWords() {
         int words = new CountLetters().count("cats|dogs");
+        Assertions.assertEquals(2, words);
+    }
+
+    @Test
+    public void multipleMatchingRSWords() {
+        int words = new CountLetters().count("catr|dogs");
         Assertions.assertEquals(2, words);
     }
 
     @Test
     public void lastWordDoesNotMatch() {
         int words = new CountLetters().count("cats|dog");
+        Assertions.assertEquals(1, words);
+    }
+
+    @Test
+    public void lastWordMatchR() {
+        int words = new CountLetters().count("cat|dogr");
         Assertions.assertEquals(1, words);
     }
 
